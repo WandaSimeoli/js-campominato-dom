36cc57a139fe062cde795d04dfcc7a7fcc2e3a09
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-22 17:47:23 +0200

    Array di bombe con ciclo while

--- js/script.js
+++ js/script.js
@@ -7,37 +7,57 @@
 
 // Creare il click del button
 
-const button = document.getElementById('click');
-
-button.addEventListener('click', function () {
-
-    // Creare la griglia numerata 
-
-    const gridNumber = 100; 
-    for ( let i = 1; i <= gridNumber; i++ ) {
-    
-    const gridContainer = document.querySelector('.grid-container');
-    const cell = document.createElement ('div');
-    gridContainer.append(cell);
-    cell.innerHTML = (i);
-
-    // Aggiungere le classi 
-    
-    cell.classList.add('cell');
-    
-    // Attivare il toggle al click
-    cell.addEventListener ('click', function () {
-        cell.classList.toggle('bg-toggle');
-        console.log(i);
-    });
-    
-    };
-    
-}); 
+ const button = document.getElementById('click');
+ button.addEventListener('click', function () {
+     // Creare la griglia numerata 
+     const gridNumber = 100; 
+     for ( let i = 1; i <= gridNumber; i++ ) {
+ 
+     const gridContainer = document.querySelector('.grid-container');
+     const cell = document.createElement ('div');
+     gridContainer.append(cell);
+     cell.innerHTML = (i);
+     // Aggiungere le classi 
+ 
+     cell.classList.add('cell');
+ 
+     // Attivare il toggle al click
+     cell.addEventListener ('click', function () {
+         cell.classList.toggle('bg-toggle');
+         console.log(i);
+     });
+ 
+     };
+ 
+ }); 
 
 
+// Creare un array di bombe 
+
+const bomb = 162; 
+const randomNumber = [];
+
+let j = 0;
+while (randomNumber.length < bomb) {
+
+    const singleNumber = getRndInteger(1,bomb);
+    console.log(singleNumber, typeof singleNumber);
+
+    if (!randomNumber.includes(singleNumber)) {
+
+        randomNumber.push(singleNumber);
+    }
+       j++;
+    }
+
+console.log('numeri casuali', randomNumber, typeof randomNumber);
 
 
+// Funzione per generare numeri casuali
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
 
 
     
